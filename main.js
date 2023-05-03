9abf8024b9a5673d742c35741440517438ae5c2f
Huỳnh Văn Đệ <86212381+huynhvande68@users.noreply.github.com>
2023-05-03 15:53:58 +0700

    update main

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const AccountRouter = require('./routers/AccountRouter')
-const app = express();
+const app = express.Router()
 const {check,validationResult} = require('express-validator')
+const db = require('../db');
+const bcrypt = require('bcrypt');
 
-app.set('view engine', 'ejs')
+app.use(express.urlencoded())
 
-const validator = [
+const loginValidator = [
+    check('email').exists().withMessage('Vui lòng nhập email người dùng')
+    .notEmpty().withMessage('Không được để trống email người dùng')
+    .isEmail().withMessage('Đây không phải là email hợp lệ'),
+
+    check('password').exists().withMessage('Vui lòng nhập mật khẩu người dùng')
+    .notEmpty().withMessage('Không được để trống mật khẩu người dùng')
+    .isLength({min: 6}).withMessage('Mật khẩu người dùng phải từ 6 kí tự'),
+
+]
+
+const registerValidator = [
     check('name').exists().withMessage('Vui lòng nhập tên người dùng')
     .notEmpty().withMessage('Không được để trống tên người dùng')
     .isLength({min: 6}).withMessage('Tên người dùng phải từ 6 kí tự'),
@@ -28,13 +40,141 @@ const validator = [
     })
 ]
 
+app.get('/logout', (req, res) => {
+    //req.session.user = null;
+    req.session.destroy();
+    res.redirect('/account/login');
+});
+app.get('/login' ,(req,res) => {
+    if(req.session.user){
+        return res.redirect('/');
+    }
 
-app.get('/', (req, res) => res.render('index'));
-app.post('/register',validator ,(req,res) => {
-    res.render('register');
 
+    const error = req.flash('error') || "";
+    const password = req.flash('password') || "";
+    const email = req.flash('email') || "";
+
+    res.render('login', {error , password , email });
 
 });
-app.get('/login', (req, res) => res.render('login'));
-app.use('/account', AccountRouter);
-app.listen(12346, () => console.log('http://localhost:12346'))
+
+app.post('/login' ,loginValidator, (req,res) => {
+    let result = validationResult(req);
+    if(result.errors.length === 0 ){
+        const {email, password} = req.body;
+        
+        const sql = 'SELECT * FROM account WHERE email = ?'
+        const params = [email];
+
+        db.query(sql, params, (err, results, fields) => {
+            if(err){
+
+                req.flash('error',err.message);
+                req.flash('password',password);
+                req.flash('email',email);
+                res.redirect('/account/login');
+            }
+            else if(results.length === 0){
+                req.flash('error','Email không tồn tại');
+                req.flash('password',password);
+                req.flash('email',email);
+                res.redirect('/account/login');
+            }else{
+                const hashed = results[0].password
+                const match = bcrypt.compareSync(password, hashed);
+                if(!match){
+                    req.flash('error','Mật khẩu không chính xác');
+                    req.flash('password',password);
+                    req.flash('email',email);
+                    res.redirect('/account/login');
+                
+                }else{
+                    //delete results[0].password
+                    req.session.user =  results[0];
+                    return res.redirect('/');
+                }
+                
+
+            }
+        })
+    }
+    else{
+        result = result.mapped()
+        console.log(result);
+
+        let message;
+        for(fields in result){
+            message = result[fields].msg;
+            break;
+        }   
+        const { email, password} = req.body;
+
+        req.flash('error',message);
+        req.flash('password',password);
+        req.flash('email',email);
+
+        res.redirect('/account/login');
+    }
+});
+
+app.get('/register' ,(req,res) => {
+    const error = req.flash('error') || "";
+    const name = req.flash('name') || "";
+    const email = req.flash('email') || "";
+
+    res.render('register', {error : error, name : name, email : email});
+
+});
+app.post('/register',registerValidator ,(req,res) => {
+    let result = validationResult(req);
+
+    if(result.errors.length === 0){
+        const {name, email, password} = req.body;
+
+        const hashed = bcrypt.hashSync(password, 10);
+
+        const sql = 'insert into account(name, email, password) values(?,?,?)';
+        const params = [name,email, hashed];
+
+        db.query(sql, params, (err, result, fields) => {
+            if(err){
+                
+                req.flash('error',err.message);
+                req.flash('name',name);
+                req.flash('email',email);
+
+                return res.redirect('/account/register');
+            }
+            else if(result.affectedRows === 1 ){
+                req.flash('success','Đăng kí thành công');
+                return res.redirect('/account/login');
+            }
+            else{
+                req.flash('error','Đăng kí thất bại');
+                req.flash('name',name);
+                req.flash('email',email);
+
+                 return res.redirect('/account/register');
+            }
+            
+        })
+    }else{
+        result = result.mapped()
+        console.log(result);
+
+        let message;
+        for(fields in result){
+            message = result[fields].msg;
+            break;
+        }   
+        const {name, email, password} = req.body;
+
+        req.flash('error',message);
+        req.flash('name',name);
+        req.flash('email',email);
+        res.redirect('/account/register');
+    }
+    
+});
+module.exports = app
